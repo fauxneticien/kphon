a4cfbcc40baaea0e91f98a2d83343724933ab92b
Nay San <nay.san@anu.edu.au>
2018-08-22 13:21:04 +1000

    Creates _kphon-export folder if it does not exist

--- R/write_kphon_exports.R
+++ R/write_kphon_exports.R
@@ -17,12 +17,18 @@ write_kphon_exports <- function(kphon_df, path, overwrite = FALSE, ffmpeg_path =
 
     stopifnot(dir.exists(path))
 
+    export_path <- normalizePath(file.path(path, "_kphon-export"))
+
+    if(!dir.exists(export_path)) {
+        dir.create(export_path)
+    }
+
     # Write csv file first
     select(kphon_df, orthography, gl_annotated, transcription, annotator, kphon_export = output_wav, annotation_source, xmin, xmax) %>%
-    write_csv(file.path(path, "_kphon-export", "_kphon-export.csv"))
+    write_csv(file.path(export_path, "_kphon-export.csv"))
 
     # Make ffmpeg commands
-    glue_data(kphon_df, "{ffmpeg_path} {ifelse(overwrite, '-y', '-n')} -i '{normalizePath(input_wav)}' -ss {round(xmin, 3)} -t {round(xmax - xmin, 3)} -ac 1 '{file.path(normalizePath(path), '_kphon-export', output_wav)}'") %>%
+    glue_data(kphon_df, "{ffmpeg_path} {ifelse(overwrite, '-y', '-n')} -i '{normalizePath(input_wav)}' -ss {round(xmin, 3)} -t {round(xmax - xmin, 3)} -ac 1 '{file.path(export_path, output_wav)}'") %>%
     walk(system)
 
 }
